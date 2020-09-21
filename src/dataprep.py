263226458960561cd3840c66f694b5439b233ea9
Adam Roth <aroth605@gmail.com>
2020-09-21 13:13:48 -0700

    works now

--- src/dataprep.py
+++ src/dataprep.py
@@ -8,17 +8,19 @@ from sklearn.preprocessing import StandardScaler
 
 
 def prep_df():
+    
     data_path = 'data/WA_Fn-UseC_-Telco-Customer-Churn.csv'
     df = pd.read_csv(data_path)
     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors = 'coerce')
     #This is a string for some ungodly reason
+    
     encode_df = df.drop(['customerID', 'tenure', 'MonthlyCharges', 'TotalCharges'],  axis = 1)
     le = LabelEncoder()
     encoded_df = encode_df.apply(le.fit_transform)
+    
     ss = StandardScaler()
     num_cols = df[['tenure', 'MonthlyCharges', 'TotalCharges']]
-    ss.fit_transform(num_cols)
-    encoded_df[['tenure', 'MonthlyCharges', 'TotalCharges']] = num_cols
-    #df[['tenure', 'MonthlyCharges', 'TotalCharges']]
+    num_cols_ss = pd.DataFrame(ss.fit_transform(num_cols), columns = num_cols.columns)
+    encoded_df[['tenure', 'MonthlyCharges', 'TotalCharges']] = num_cols_ss
     
     return encoded_df
